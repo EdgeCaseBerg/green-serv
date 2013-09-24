04983852dc67ad882a5f6b9ba63923ad3686f1f1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-09-23 21:14:34 -0400

    Refactored connection into function
    Moved connection into it's own function to make code cleaner.

--- src/db.c
+++ src/db.c
@@ -1,23 +1,32 @@
 #include "db.h"
 #include "config.h"
 
-int main() {
+MYSQL * getMySQLConnection(){
    MYSQL *conn;
-   MYSQL_RES *res;
-   MYSQL_ROW row;
    char *server = HOST;
    char *user = USERNAME;
    char *password = PASSWORD; /* set me first */
    char *database = DATABASE;
-   conn = mysql_init(NULL);
-   /* Connect to database */
    
-   if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
+   /* Call to init for multi-threading later on */
+   mysql_library_init(0, NULL, NULL);
+
+   conn = mysql_init(NULL);
+   return mysql_real_connect(conn, server, user, password, database, 0, NULL, 0);
+}
+
+int main() {
+   MYSQL *conn;
+   MYSQL_RES *res;
+   MYSQL_ROW row;
+
+   conn = getMySQLConnection();
+   
+   if (!conn) {
       fprintf(stderr, "%s\n", mysql_error(conn));
       exit(1);
    }
 
- 
    /* send SQL query */
    if (mysql_query(conn, "show tables")) {
       fprintf(stderr, "%s\n", mysql_error(conn));
@@ -31,6 +40,8 @@ int main() {
    /* close connection */
    mysql_free_result(res);
    mysql_close(conn);
+
+   /*Do this or leak. */
    mysql_library_end();
    return 0;
 }
