ef42eb2853c12c67f0738032916181307f85ecb5
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-02 08:34:31 -0400

    Update scope with defined size for description
    md5sum a.out
    63f7112975bd611e01d9bebf3ef933ba  a.out
    
    Added define instead of magic number for description size within the
    scope model. This is to help facilitate proper buffer safety.

--- src/models/scope.c
+++ src/models/scope.c
@@ -11,12 +11,12 @@ void gs_scope_setId(long id, struct gs_scope * gss){
 
 /* Note that only 8 characters will ever be copied from desc*/
 void gs_scope_setDesc(char * desc, struct gs_scope * gss){
-	bzero(gss->description,9);
-	strncpy(gss->description,desc,8);
+	bzero(gss->description,GS_SCOPE_DESCRIPTION_SIZE);
+	strncpy(gss->description,desc,GS_SCOPE_DESCRIPTION_SIZE-1);
 }
 
 /* Clears out a scope structure to make it ready for use.*/
 void gs_scope_ZeroStruct(struct gs_scope * gss){
-	bzero(gss->description,9);
+	bzero(gss->description,GS_SCOPE_DESCRIPTION_SIZE);
 	gss->id = GS_SCOPE_INVALID_ID; 
 }
