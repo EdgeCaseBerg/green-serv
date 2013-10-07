029da5d6883f1531131557898acdef1455f0a586
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-06 22:25:18 -0400

    More networking basics
    -- no md5sum --
    
    Got a ragtag http parsing function to populate a simple http request
    struct that stores the information I care about. I think it could
    probably be made simple if I made the assumption that incoming requests
    would be valid http all the time. But that seems a dangerous assumption
    to make.
    
    It is working right now though with no real trouble which is rather
    nice. If I can abstract the core pieces of this code into their own
    functions then I might get the beginning of some network code the main
    driver function can use to determine controller calling.
    
    for example, with a GET and a url of "","/", or "/api" I could redirect
    to the heartbeat.

---
+++ headers/network/net.h
@@ -0,0 +1,38 @@
+#ifndef __NET_H__
+	#define __NET_H__
+	/*Create an HTTP Json response
+	*/
+	int createResponse(char * content, char * buff, int status);
+
+	/*Create Socket for communications
+ 	*returns file descriptor or -1 on err
+	*/
+	int createSocket();
+
+	/* Sets up a socket for use as a server socket on the designated port
+	 * returns -1 on errors, 0 otherwise
+	 * fd is the file descriptor of the socket to setup
+	 * sockserv is a pointer to a sockaddr_in struct.
+	 * port is the port to bind the address to
+	*/
+	int setupSockAndBind(int fd, struct sockaddr_in * sockserv, int port );
+
+	/* This function is unlikely to live beyond testing things */
+	int test_network(char * buffer, int bufferLength);
+
+	#define METHOD_LENGTH 7
+	#define MAX_URL_LENGTH 100
+	#define FIRSTLINEBUFFSIZE 256
+	#define GET 2
+	#define POST 4
+	#define PUT 8
+	#define DELETE 16
+	#define UNKNOWN_METHOD 32
+	struct http_request{
+		int method;
+		char url[MAX_URL_LENGTH]; /* The request URL to determine the controller */
+		int contentLength; /* The length of the content coming in */
+		char * data; /* This must be malloced for data */
+	};
+
+#endif 
--- src/network/net.c
+++ src/network/net.c
@@ -7,8 +7,9 @@
 #include <fcntl.h>
 #include <time.h>
 
+#include "network/net.h"
 
-int createResponse(char * content, char * buff){
+int createResponse(char * content, char * buff, int status){
     char timeBuffer[1000];
     time_t now;
     struct tm tm;
@@ -18,11 +19,12 @@ int createResponse(char * content, char * buff){
     tm  = *gmtime(&now);
     strftime(timeBuffer, sizeof timeBuffer, "%a, %d %b %Y %H:%M:%S %Z", &tm);
     return sprintf(buff,  
-                    "HTTP/1.1 200 OK\r\n"
+                    "HTTP/1.1 %d OK\r\n"
                    "Content-Type: application/json\r\n"
                    "%sr\n"
                    "Connection: close\r\n"
                    "Content-Length: %zu\r\n\r\n%s\r\n",
+                   status,
                    timeBuffer,
                    strlen(content),
                    content);
@@ -49,13 +51,145 @@ int setupSockAndBind(int fd, struct sockaddr_in * sockserv, int port ){
     return bind(fd,(struct sockaddr *)sockserv,sizeof(*sockserv));
 }
 
+static int strnstr(char * needle, char * haystack, int haystackLen){
+    int i;
+    int j;
+    int bad;
+    int needleLen = strlen(needle);
+    for(i=0; i < haystackLen && haystack[i] != '\0'; ++i){
+        if( needle[0] == haystack[i] ){
+            j=i;
+            while(needle[j-i] == haystack[j] && j-i < needleLen)
+                j++;
+            if(j-i == needleLen){
+                /* We made it through the haystack and
+                 * and looked at all the characters without
+                 * terminating early. 
+                */
+                 printf("found %s:%d\n",needle, i);
+                 return i;
+            }else{
+                bad = 0;
+            }
+        }
+    }
+    printf("%s%s\n", "could not find ", needle);
+    return -1;
+}
+
+int parseRequest(struct http_request * requestToFill, char * requestStr){
+    char buff[FIRSTLINEBUFFSIZE]; /* This is the most we'll read */
+    int i;
+    int methodLoc;
+    int urlStart;
+    int urlEnd;
+    int contentLength;
+    bzero(buff,FIRSTLINEBUFFSIZE);
+    bzero(requestToFill->url,MAX_URL_LENGTH);
+    requestToFill->url[0] = '/'; /* Default assumption */
+
+    /* Attempt to determine the request method */
+    for(i=0; i <  FIRSTLINEBUFFSIZE && requestStr[i] != '\0'; ++i){
+        if(requestStr[i] == '\r')
+            if(requestStr[i+1] == '\n')
+                break;
+        /* Copy copy copy... */
+        buff[i] = requestStr[i];
+    }
+    methodLoc = strnstr("GET", buff, FIRSTLINEBUFFSIZE  );
+    if(methodLoc == -1){
+        methodLoc = strnstr("POST", buff, FIRSTLINEBUFFSIZE );
+        if(methodLoc == -1){
+            methodLoc = strnstr("PUT", buff, FIRSTLINEBUFFSIZE  );
+            if(methodLoc == -1){
+                methodLoc = strnstr("DELETE", buff, FIRSTLINEBUFFSIZE   );
+                if(methodLoc == -1){
+                    /* Fuck it. What'd you try to give me? */
+                    fprintf(stderr, "Could not determine method: %s\n",buff );
+                    requestToFill->method = UNKNOWN_METHOD;
+                }else{
+                    requestToFill->method = DELETE;
+                    methodLoc+= 7; /* Advance past the method and space */
+                }
+            }else{
+                requestToFill->method = PUT;
+                methodLoc+= 4;
+            }
+        }else{
+            requestToFill->method = POST;
+            methodLoc+=5;
+        }
+    }else{
+        requestToFill->method = GET;
+        methodLoc+=4;
+    }
+    urlStart = methodLoc;
+    printf("urlStart %d\n", urlStart);
+    printf("%s\n", buff);
+    urlEnd = strnstr("HTTP", buff, FIRSTLINEBUFFSIZE );
+    /* Find the url (Which will be between the method to the http version)*/
+    i=methodLoc;
+    for(methodLoc=0; methodLoc < MAX_URL_LENGTH && i < FIRSTLINEBUFFSIZE && requestStr[i] != '\0' && i < urlEnd; ++i){
+        if(requestStr[i] != ' '){
+            requestToFill->url[methodLoc] = requestStr[i];
+            methodLoc++; /* Just re-using a variable instead of using a new one */
+        }
+    }
+    /* Find out if there's any content: */
+    contentLength = strnstr("Content-Length",requestStr,strlen(requestStr));
+    if(contentLength != -1){
+        /*Determine actual content length */
+        contentLength+=strlen("Content-Length: "); /* Skip the text */
+        methodLoc=0; /* ReUsing again */
+        for(i=contentLength; i < FIRSTLINEBUFFSIZE && requestStr[i] != '\0' && requestStr[i] != '\r'; ++i)
+            buff[methodLoc++] = requestStr[i];
+        buff[i] = '\0';
+        /* Attempt to parse: */
+        contentLength = atoi(buff);
+
+        if(contentLength > 0){
+            /* If there's data than place it into the structure */
+            if(requestToFill->method == POST || requestToFill->method == PUT){
+                requestToFill->data = malloc(contentLength*sizeof(char)+1);
+                if(requestToFill->data != NULL){
+                    /* Find the content: */
+                    i = strnstr("\r\n\r\n", requestStr, strlen(requestStr));
+                    if(i != -1){
+                        i+=4; /* skip newlines */
+                        for(methodLoc = 0; i < strlen(requestStr) && requestStr[i] != '\0'; ++i, ++methodLoc)
+                            requestToFill->data[methodLoc] = requestStr[i];
+                        requestToFill->data[methodLoc] = '\0';
+                    }else{
+                        /* Could not find content...*/
+                        free(requestToFill->data);
+                        fprintf(stderr, "%s\n", "Could not find start of content. memory freed");
+                    }
+                }else{
+                    fprintf(stderr, "%s\n", "Could not allocate enough memory for content in request ");
+                }
+            }else{
+                /* Don't preallocate memory for the struct or this will cause
+                 * memory not being able to be reached
+                */
+                requestToFill->data = NULL;
+            }
+        }
+    }
+    /* Calling parties must use contentLenght to tell if they will need to free the memory */
+    requestToFill->contentLength = contentLength;
+    return contentLength;
+
+    
+}
+
 /* Just a test function... */
-int test(){
+int test_network(char * buffer, int bufferLength){
     struct sockaddr_in sockserv,sockclient;
     int socketfd,clientfd;
     socklen_t clientsocklen;
     int bytesSent = clientfd = socketfd = 0; 
     char buff[BUFSIZ];
+    struct http_request request;
 
     bzero(buff,BUFSIZ);
     bzero(&sockserv,sizeof(sockserv));
@@ -77,7 +211,20 @@ int test(){
         buff[read(clientfd,buff,BUFSIZ)] = '\0';
         printf("Request recieved as \n%s\n",buff);  
 
-        createResponse("{}",buff);
+        strncpy(buffer, buff ,bufferLength);
+
+        /* We'd actually be interupted here and go do things based on the buffer.
+         * But for now we just copy it into the string and send back a 200
+        */
+        parseRequest(&request, buff);
+        printf("url: %s\n", request.url);
+        if(request.contentLength > 0)
+            printf("data: %s\n", request.data);
+        if(request.contentLength > 0)
+            free(request.data);
+
+
+        createResponse("{}",buff,200);
         buff[strlen(buff)] = '\0';
 
         bytesSent = send(clientfd,buff,strlen(buff),0);  
@@ -89,4 +236,10 @@ int test(){
     close(socketfd);
 
     return 0;
+}
+
+int main(){
+    char buff[1024];
+    bzero(buff,1024);
+    test_network(buff,1024);
 }
