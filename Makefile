2692c31d55124be4740ca86afe19575c16084c3c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-08 18:11:20 -0400

    Router function Unit Tests
    md5sum a.out
    a7ffb4868abce6acc937b630ce5086d3  a.out
    
    Added testing for the router's function to determine the controller to
    call. It's passing with flying colors now that I've redone some of the
    logic in it. The urllength parameter should be removed since it's unused
    entirely. I originally had it in place to make sure I didn't do anything
    strange with overruning the buffers or losing parameters, but the min
    was causing false positives for a lot of things. Not too mention that I
    wasn't checking the return of the strncasecmp correctly... (for some
    reason I forgot that string comparison functions return 0 on equal)
    
    After fixing the router and making the main program, the server responds
    with either 200 or 404 depending on if it recognizes the route or not.
    This is a great step forward.

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ gflags = -I./headers -std=gnu99 -pedantic -Wall -Wextra -Werror -g
 mysqlflags = -I/usr/include/mysql -DBIG_JOINS=1 -fno-strict-aliasing
 mysqllibs  = -L/usr/lib/x86_64-linux-gnu -lmysqlclient -lpthread -lz -lm -lrt -ldl
 valgrind = valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes
-unittests = test-decimal test-comment test-scope test-marker test-report test-heatmap test-heartbeat
+unittests = test-decimal test-comment test-scope test-marker test-report test-heatmap test-heartbeat test-router
 unittestobj = obj/comment.o  obj/db.o  obj/decimal.o  obj/heatmap.o  obj/json.o obj/marker.o  obj/report.o  obj/scope.o  obj/sha256.o
 controllertests = test-hb-controller
 
@@ -72,6 +72,7 @@ units: $(unittests)
 	$(valgrind) tests/bin/report.out
 	$(valgrind) tests/bin/heatmap.out
 	$(valgrind) tests/bin/heartbeat.out
+	$(valgrind) tests/bin/router.out
 
 test-decimal: tests/unit/decimal-test.c decimal.o
 	$(CC) $(gflags) tests/unit/decimal-test.c obj/decimal.o -o tests/bin/decimal.out -lm -rdynamic
@@ -93,6 +94,9 @@ test-heatmap: tests/unit/heatmap-test.c heatmap.o json.o db.o decimal.o
 
 test-heartbeat: tests/unit/heartbeat-test.c json.o decimal.o
 	$(CC) $(gflags) tests/unit/heartbeat-test.c obj/json.o obj/decimal.o -o tests/bin/heartbeat.out 
+
+test-router: tests/unit/router-test.c router.o
+	$(CC) $(gflags) tests/unit/router-test.c obj/router.o -o tests/bin/router.out
 
 #Controller Tests
 
--- src/network/router.c
+++ src/network/router.c
@@ -9,23 +9,24 @@ int determineController(char * url, int urlLength){
 	 * show up. This is not a regex after all. (although we could prob
 	 *-ably use strstr...)
 	*/
-	if(strncasecmp(url, "/api/comments",min(13,urlLength))			||
-			strncasecmp(url,"/api/comments/",min(14,urlLength))	){
+urlLength+=1;
+	if(strncasecmp(url, "/api/comments",13 ) == 0		||
+			strncasecmp(url,"/api/comments/",14 ) == 0	){
 			/* Comments Controller */
 			return 	COMMENTS_CONTROLLER;
-	}else if(strncasecmp(url, "/api/heatmap",min(12,urlLength))	||
-			strncasecmp(url, "/api/heatmap/",min(13,urlLength))	){
+	}else if(strncasecmp(url, "/api/heatmap",12 ) == 0	||
+			strncasecmp(url, "/api/heatmap/",13 ) == 0	){
 			return HEATMAP_CONTROLLER;
-	}else if(strncasecmp(url,"/api/pins",min(9,urlLength))			||
-			strncasecmp(url,"/api/pins/",min(10,urlLength))		){
+	}else if(strncasecmp(url,"/api/pins",9 ) == 0		||
+			strncasecmp(url,"/api/pins/",10 ) == 0		){
 			return MARKER_CONTROLLER;
-	}else if(strncasecmp(url,"/api/debug",min(10,urlLength)) 		||
-			strncasecmp(url,"/api/debug/",min(11,urlLength))		){
+	}else if(strncasecmp(url,"/api/debug",10 ) == 0 	||
+			strncasecmp(url,"/api/debug/",11 ) == 0		){
 			return REPORT_CONTROLLER;
-	}else if(strncasecmp(url, "",min(1,urlLength)) 		||
-			strncasecmp(url, "/",min(1,urlLength))		||
-			strncasecmp(url, "/api/",min(5,urlLength))	||
-			strncasecmp(url, "/api", min(4,urlLength))	){
+	}else if(strcasecmp(url, "") == 0 		||
+			strcasecmp(url, "/" ) == 0		||
+			strcasecmp(url, "/api/" ) == 0	||
+			strcasecmp(url, "/api" ) == 0	){
 			/* HeartBeat Controller */
 			return HEARTBEAT_CONTROLLER;
 	}/* No Route matches. */
