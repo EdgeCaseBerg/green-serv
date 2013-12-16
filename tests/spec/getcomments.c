3eafa1c6bf69b06b707b0041842a346acfc58bd0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-12-16 18:06:48 -0500

    Refactored getcomments spec test
    md5sum a.out
    d503e4ca9b18de9089d36fcb6a2988a8  a.out
    
    Changed GET comments spec test to use a macro for easier readability.

--- tests/spec/getcomments.c
+++ tests/spec/getcomments.c
@@ -2,123 +2,83 @@
 #include "controllers/comments.h"
 #include "network/router.h"
 
+#define EXPECTED(expected, status, errmessage) \
+	if( expected == status ) \
+		fprintf(stdout, "." );\
+	else{\
+		fprintf(stdout, "F" );\
+		fprintf(stderr, "GET COMMENTS: Expected status of %d, recieved %d. %s\n", expected,status, errmessage );\
+	}
+
 int main(){
 	char * stringToReturn;
 	struct http_request request;
 	int status;
+	int size;
 
+	size = 1000;
 	request.method = GET;
 	sprintf(request.url, "/api/comments");
 	
-	stringToReturn = malloc(1000);
+	stringToReturn = malloc(size);
 
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Default GET failed")
 	
 	/* We could automate the requests below by putting the types into a loop 
 	 * and then looping over them, but we're testing so we want to be as explicit
 	 * as possible.
 	*/
 	sprintf(request.url, "/api/comments?type=COMMENT");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200 when queried for type=comment\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Queried for type=comment")
+	
 
 	sprintf(request.url, "/api/comments?type=ADMIN");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200 when queried for type=admin\n" );
-	}	
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Queried for type=admin")
+	
 
 	sprintf(request.url, "/api/comments?type=MARKER");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200 when queried for type=marker\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Queried for type=marker")
+	
 
 	sprintf(request.url, "/api/comments?page=1");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200,status, "Queried for page=1")
 
 
 	sprintf(request.url, "/api/comments?type=COMMENT&page=1");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200 when queried for type=comment\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Queried for type=comment")
+	
 
 	sprintf(request.url, "/api/comments?type=ADMIN&page=1");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200 when queried for type=admin\n" );
-	}	
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Queried for type=admin")
+	
 
 	sprintf(request.url, "/api/comments?type=MARKER&page=1");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 200 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 200 when queried for type=marker\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(200, status, "Queried for type=marker and page=1")
 
 
 	/* Test invalid GETs */
 	sprintf(request.url, "/api/comments?type=bad");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 422 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 422 when queried with bad type\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(422, status, "Queried with bad type")
+	
 
 	sprintf(request.url, "api/comments?page=-1");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 422 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 422 when queried with negative page\n" );
-	}
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(422, status, "Queried with negative page value")
+	
 
 	sprintf(request.url, "api/comments?page=derp");
-	status = comment_controller(&request, stringToReturn, 1000);
-	if( status == 422 )
-		fprintf(stdout, "." );
-	else{
-		fprintf(stdout, "F" );
-		fprintf(stderr, "GET COMMENTS: request failed to return status of 422 when queried with alphabetical page\n" );
-	}
-
-
-		
+	status = comment_controller(&request, stringToReturn, size);
+	EXPECTED(422, status, "Queried with alphabetical page instead of number")
+			
 
 	fflush(stdout);
 	fflush(stderr);
@@ -127,3 +87,5 @@ int main(){
 	close(STDERR_FILENO);
 	return 0;
 }
+
+#undef EXPECTED
